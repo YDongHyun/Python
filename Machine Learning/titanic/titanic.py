df75f93aa1c7b3bb5555dac70ab5d5cc75bf5369
양동현 <80799025+YDongHyun@users.noreply.github.com>
2021-12-30 22:57:32 +0900

    Update titanic.py

--- Machine Learning/titanic/titanic.py
+++ Machine Learning/titanic/titanic.py
@@ -1,1 +1,19 @@
+#데이터 분석 연습
+
+import pandas as pd
+
+titanic_df=pd.read_csv('G:\\python\\test\\titanic_train.csv')
+#print(titanic_df.head(3))
+
+#print(titanic_df.shape)
+
+#print(titanic_df.info)
+
+print(titanic_df['Survived'].value_counts())
+
+print(titanic_df['Survived'].count())
+
+titanic_df.head()
+titanic_df['Age_0'] = 2
+print(titanic_df.head())
 
